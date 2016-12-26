683f26ac09471179727f30f30f1b15d68c388228
Bruno Santos <bruno.santos1996@gmail.com>
2016-12-26 15:47:14 +0000

    interface visuals done

--- reader/primitives/Box.js
+++ reader/primitives/Box.js
@@ -20,11 +20,4 @@ Box.prototype.display= function() {
 		this.scene.scale(this.base_height, 1, this.base_height);
 		this.side.display();
 	this.scene.popMatrix();
-	
-	/*
-	this.scene.pushMatrix();
-		this.scene.translate(-(this.base_width/2-this.base_height/2), 0, this.base_height-this.base_z);
-		this.scene.scale(this.base_height, 1, this.base_height);
-		this.side.display();
-	this.scene.popMatrix();*/
 }
